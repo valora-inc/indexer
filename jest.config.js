a60c7e22df9fb967e5059593a582fa056a1fc355
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-08-10 09:44:08 -0700

    Fix test config and add CI workflow

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
-const { nodeFlakeTracking } = require('@celo/flake-tracker/src/jest/config.js')
-
 module.exports = {
-  ...nodeFlakeTracking,
+  watchman: false,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts', ...nodeFlakeTracking.setupFilesAfterEnv],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/?(*.)(spec|test).ts?(x)'],
   testResultsProcessor: 'jest-junit',
   transform: {
