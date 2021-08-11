8b91bb130eb6640a5964f30e4c2352bb325b6229
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-08-10 20:25:04 -0700

    Error if WEB3_PROVIDER_URL is missing

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import { initDatabase } from './database/db'
 import { pollers } from './polling'
 
 export default async function initApp() : Promise<express.Application> {
+  if (!WEB3_PROVIDER_URL) {
+    throw new Error('You must set the WEB3_PROVIDER_URL environment variable.')
+  }
+
   console.info('Service starting with environment, version:', ENVIRONMENT, VERSION)
   const START_TIME = Date.now()
 
@@ -39,10 +43,6 @@ export default async function initApp() : Promise<express.Application> {
   } catch (error) {
     console.error('Error initializing database', error)
     throw error
-  }
-  if (!WEB3_PROVIDER_URL) {
-    console.info('No Web3 provider found. Skipping exchange polling.')
-    console.info('Note that you will need to manually set contract addresses.')
   }
 
   return app
