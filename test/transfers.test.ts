7acac8212c61f56fa5b00245cfe66e4b26c547a4
Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
2022-12-07 17:45:45 -0800

    test(transfers): tests for handleTransfers (#26)
    * test(transfers): tests for handleTransfers
    
    mocks indexEvents to make sure it's a unit test
    
    * added code comment

---
+++ test/transfers.test.ts
@@ -0,0 +1,50 @@
+import { indexEvents } from '../src/indexer'
+import { mocked } from 'ts-jest/utils'
+import { handleTransfers } from '../src/indexer/transfers'
+
+jest.mock('../src/indexer')
+
+describe('handleTransfers', () => {
+  it('Indexes events for all supported tokens', async () => {
+    const mockPayload = {
+      returnValues: {
+        from: '0x123',
+        to: '0xabc',
+        value: '456',
+      },
+    }
+    const contractToCurrency = {
+      // uses hardcoded keys like cUsd instead of Contract.cUsd because these shouldn't actually change. They're saved
+      //  to the DB and used in queries. So if a value in Contract changes, we want a test to fail.
+      cUsd: 'cUSD',
+      cEur: 'cEUR',
+      cReal: 'cREAL',
+      mCUsd: 'mCUSD',
+      mCEur: 'mCEUR',
+      mCReal: 'mCREAL',
+    }
+    const indexEventsMock = jest
+      .fn()
+      .mockImplementation((contract, _event, _tableName, payloadMapper) => {
+        const mappedPayload = payloadMapper(mockPayload)
+        if (!(contract in contractToCurrency)) {
+          throw new Error(
+            `Contract ${contract} not found in contractToCurrency. Make sure to add it to get coverage on labels for indexed data.`,
+          )
+        }
+        const expectedCurrency =
+          contractToCurrency[contract as keyof typeof contractToCurrency]
+        expect(mappedPayload?.currency).toEqual(expectedCurrency)
+      })
+    mocked(indexEvents).mockImplementation(indexEventsMock)
+    await handleTransfers()
+    Object.keys(contractToCurrency).forEach((contract) => {
+      expect(indexEventsMock).toHaveBeenCalledWith(
+        contract,
+        'Transfer',
+        'transfers',
+        expect.any(Function),
+      )
+    })
+  })
+})
