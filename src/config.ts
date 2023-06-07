17440a2b961863a4766c2ca7c59c54119cedbaa5
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-06-07 16:58:04 -0700

    remove unused configs

--- src/config.ts
+++ src/config.ts
@@ -20,10 +20,6 @@ export const BLOCK_METADATA_POLLING_INTERVAL =
   Number(process.env.BLOCK_METADATA_POLLING_INTERVAL) || 5000
 export const BLOCK_METADATA_DEFAULT_MIN_BLOCK_NUMBER =
   Number(process.env.BLOCK_METADATA_DEFAULT_MIN_BLOCK_NUMBER) || 19546425 // arbitrarily chosen from before rate limiting outage
-export const BLOCK_METADATA_POOL_LIMIT =
-  Number(process.env.BLOCK_METADATA_POOL_LIMIT) || 100 // QuickNode limits us to 300 requests/second. leaves buffer for other kinds of RPC requests
-export const BLOCK_METADATA_MIN_BATCH_PERIOD_MS =
-  Number(process.env.BLOCK_METADATA_MIN_BATCH_PERIOD_MS) || 1000 // minimum time to process 1 batch. increase if hitting rate limits with RPC provider.
 
 export const WEB3_PROVIDER_URL = process.env.WEB3_PROVIDER_URL || 'UNDEFINED'
 
