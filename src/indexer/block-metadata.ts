9c131e335f433fd5f352e38e711a9b29a43b488e
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-06-07 16:55:22 -0700

    remove unnecessary performance optimizations from block-metadata.ts

--- src/indexer/block-metadata.ts
+++ src/indexer/block-metadata.ts
@@ -1,13 +1,6 @@
 import { database } from '../database/db'
-import * as _ from 'lodash'
-import asyncPool from 'tiny-async-pool'
 import { getContractKit } from '../util/utils'
-import { sleep } from '../util/sleep'
-import {
-  BLOCK_METADATA_DEFAULT_MIN_BLOCK_NUMBER,
-  BLOCK_METADATA_MIN_BATCH_PERIOD_MS,
-  BLOCK_METADATA_POOL_LIMIT,
-} from '../config'
+import { BLOCK_METADATA_DEFAULT_MIN_BLOCK_NUMBER } from '../config'
 
 const TABLE_NAME = 'block_metadata'
 
@@ -19,41 +12,24 @@ export async function handleBlockMetadata() {
     const lastBlockNumberIndexed =
       (await database(TABLE_NAME).max('blockNumber').first())?.blockNumber ??
       BLOCK_METADATA_DEFAULT_MIN_BLOCK_NUMBER
-    const fromBlock = lastBlockNumberIndexed + 1
+    let blockNumber = lastBlockNumberIndexed + 1
     console.info(
-      `Indexing block metadata between blocks [${fromBlock}, ${currentBlockNumber}]`,
+      `Indexing block metadata between blocks [${blockNumber}, ${currentBlockNumber}]`,
     )
-    await database.transaction(async (trx) => {
-      await asyncPool(
-        BLOCK_METADATA_POOL_LIMIT,
-        _.range(fromBlock, currentBlockNumber + 1),
-        async (blockNumber) => {
-          const start = Date.now()
-          const blockTimestamp = (
-            await contractKit.web3.eth.getBlock(blockNumber)
-          )?.timestamp
-          if (!blockTimestamp) {
-            throw new Error(
-              `Could not get block timestamp for block ${blockNumber}`,
-            )
-          }
-          await database(TABLE_NAME)
-            .insert({
-              blockNumber,
-              blockTimestamp,
-            })
-            .onConflict('blockNumber')
-            .ignore() // do nothing if row already exists with same blockNumber
-            .transacting(trx)
-
-          // avoid RPC rate limits
-          const elapsedMs = Date.now() - start
-          if (elapsedMs < BLOCK_METADATA_MIN_BATCH_PERIOD_MS) {
-            await sleep(BLOCK_METADATA_MIN_BATCH_PERIOD_MS - elapsedMs)
-          }
-        },
+    while (blockNumber < currentBlockNumber) {
+      const blockTimestamp = Number(
+        (await contractKit.web3.eth.getBlock(blockNumber)).timestamp,
       )
-    })
+      await database(TABLE_NAME)
+        .insert({
+          fromBlock: blockNumber,
+          blockTimestamp,
+        })
+        .onConflict('blockNumber')
+        .ignore() // do nothing if row already exists with same blockNumber
+      blockNumber++
+    }
+    console.info(`Indexed block metadata up to block ${blockNumber - 1}`)
   } catch (error) {
     console.error(`Error indexing block metadata: ${error}`)
   }
--- src/util/sleep.ts
+++
@@ -1,4 +0,0 @@
-export function sleep(ms: number): Promise<undefined> {
-  // https://stackoverflow.com/a/39914235/5807149
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
