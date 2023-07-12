b5ac62d34d689d867a78f0be126416b7d1fc96af
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-07-11 18:40:40 -0700

    add query parameter, boolean field on response to make alerting setup easier

--- src/status.ts
+++ src/status.ts
@@ -6,22 +6,32 @@ import { getContractKit } from './util/utils'
 import { Request, Response } from 'express'
 import { asyncRoute } from './util/async-route'
 
+const BLOCKS_PER_MINUTE = 12
+export const DEFAULT_MAX_BLOCKS_BEHIND = BLOCKS_PER_MINUTE * 120 // 2 hours
+
 export function getStatusHandler(startTime: number) {
-  return asyncRoute(async (_req: Request, res: Response) => {
+  return asyncRoute(async (req: Request, res: Response) => {
+    const maxBlocksBehind = req.query?.max_blocks_behind
+      ? parseInt(req.query.max_blocks_behind.toString())
+      : DEFAULT_MAX_BLOCKS_BEHIND
+
     const kit = await getContractKit()
     const curBlockNumber = await kit.web3.eth.getBlockNumber()
     const minTransferLastBlock = await database(LAST_BLOCKS_TABLE_NAME)
       .min('lastBlock as minLastBlock')
       .where('key', 'like', `%_${Event.Transfer}`)
 
+    const blocksBehind = minTransferLastBlock.length
+      ? curBlockNumber - (minTransferLastBlock[0].minLastBlock ?? 0)
+      : curBlockNumber
+
     res.status(200).json({
       version: VERSION,
       serviceStartTime: new Date(startTime).toUTCString(),
       serviceRunDuration:
         Math.floor((Date.now() - startTime) / 60000) + ' minutes',
-      blocksBehind: minTransferLastBlock.length
-        ? curBlockNumber - (minTransferLastBlock[0].minLastBlock ?? 0)
-        : curBlockNumber,
+      blocksBehind,
+      blockWithinRange: blocksBehind < maxBlocksBehind,
     })
   })
 }
--- test/status.test.ts
+++ test/status.test.ts
@@ -2,7 +2,7 @@ import { ContractKit } from '@celo/contractkit'
 import { database, initDatabase } from '../src/database/db'
 import { mocked } from 'ts-jest/utils'
 import { getContractKit } from '../src/util/utils'
-import { getStatusHandler } from '../src/status'
+import { DEFAULT_MAX_BLOCKS_BEHIND, getStatusHandler } from '../src/status'
 import { Request, Response } from 'express'
 
 jest.mock('../src/util/utils')
@@ -10,7 +10,7 @@ jest.mock('../src/config', () => ({
   VERSION: 'fake-version',
 }))
 
-describe('status', () => {
+describe('getStatusHandler', () => {
   const mockGetBlockNumber = jest.fn()
   const mockContractKit = {
     web3: {
@@ -19,13 +19,14 @@ describe('status', () => {
       },
     },
   } as unknown as ContractKit
+  const minBlock = 5
   beforeEach(async () => {
     await initDatabase()
     await database.table('last_blocks').truncate()
     await database('last_blocks').insert([
       {
         key: 'testToken1_Transfer',
-        lastBlock: 5,
+        lastBlock: minBlock,
       },
       {
         key: 'testToken2_Transfer',
@@ -38,25 +39,86 @@ describe('status', () => {
   afterEach(() => {
     return database.destroy()
   })
-  it('getStatusHandler', async () => {
-    const mockResponse = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    }
-    mockGetBlockNumber.mockResolvedValue(17)
-    const mockStartTime = 1689116816000 // epoch millis for july 11, 2023
-    Date.now = jest.fn().mockReturnValue(mockStartTime + 150000) // 2.5 minutes past start time
-    await getStatusHandler(mockStartTime)(
-      {} as unknown as Request,
-      mockResponse as unknown as Response,
-      jest.fn(),
-    )
-    expect(mockResponse.status).toBeCalledWith(200)
-    expect(mockResponse.json).toBeCalledWith({
-      version: 'fake-version',
-      serviceStartTime: 'Tue, 11 Jul 2023 23:06:56 GMT',
-      serviceRunDuration: '2 minutes',
-      blocksBehind: 12,
+  const mockResponse = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  }
+  const mockStartTime = 1689116816000 // epoch millis for july 11, 2023
+  Date.now = jest.fn().mockReturnValue(mockStartTime + 150000) // 2.5 minutes past start time
+  const mockNext = jest.fn()
+  const statusHandler = getStatusHandler(mockStartTime)
+  describe('without query parameters', () => {
+    it('works when within range', async () => {
+      mockGetBlockNumber.mockReturnValue(
+        DEFAULT_MAX_BLOCKS_BEHIND + minBlock - 1,
+      )
+      await statusHandler(
+        {} as unknown as Request,
+        mockResponse as unknown as Response,
+        mockNext,
+      )
+      expect(mockNext).not.toHaveBeenCalled()
+      expect(mockResponse.status).toBeCalledWith(200)
+      expect(mockResponse.json).toBeCalledWith({
+        version: 'fake-version',
+        serviceStartTime: 'Tue, 11 Jul 2023 23:06:56 GMT',
+        serviceRunDuration: '2 minutes',
+        blocksBehind: DEFAULT_MAX_BLOCKS_BEHIND - 1,
+        blockWithinRange: true,
+      })
+    })
+    it('works when too far behind', async () => {
+      mockGetBlockNumber.mockReturnValue(DEFAULT_MAX_BLOCKS_BEHIND + minBlock)
+      await statusHandler(
+        {} as unknown as Request,
+        mockResponse as unknown as Response,
+        mockNext,
+      )
+      expect(mockNext).not.toHaveBeenCalled()
+      expect(mockResponse.status).toBeCalledWith(200)
+      expect(mockResponse.json).toBeCalledWith({
+        version: 'fake-version',
+        serviceStartTime: 'Tue, 11 Jul 2023 23:06:56 GMT',
+        serviceRunDuration: '2 minutes',
+        blocksBehind: DEFAULT_MAX_BLOCKS_BEHIND,
+        blockWithinRange: false,
+      })
+    })
+  })
+  describe('using query parameters', () => {
+    it('works when within range', async () => {
+      mockGetBlockNumber.mockReturnValue(99 + minBlock)
+      await statusHandler(
+        { query: { max_blocks_behind: 100 } } as unknown as Request,
+        mockResponse as unknown as Response,
+        mockNext,
+      )
+      expect(mockNext).not.toHaveBeenCalled()
+      expect(mockResponse.status).toBeCalledWith(200)
+      expect(mockResponse.json).toBeCalledWith({
+        version: 'fake-version',
+        serviceStartTime: 'Tue, 11 Jul 2023 23:06:56 GMT',
+        serviceRunDuration: '2 minutes',
+        blocksBehind: 99,
+        blockWithinRange: true,
+      })
+    })
+    it('works when too far behind', async () => {
+      mockGetBlockNumber.mockReturnValue(100 + minBlock)
+      await statusHandler(
+        { query: { max_blocks_behind: 100 } } as unknown as Request,
+        mockResponse as unknown as Response,
+        mockNext,
+      )
+      expect(mockNext).not.toHaveBeenCalled()
+      expect(mockResponse.status).toBeCalledWith(200)
+      expect(mockResponse.json).toBeCalledWith({
+        version: 'fake-version',
+        serviceStartTime: 'Tue, 11 Jul 2023 23:06:56 GMT',
+        serviceRunDuration: '2 minutes',
+        blocksBehind: 100,
+        blockWithinRange: false,
+      })
     })
   })
 })
